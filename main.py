f8c10548d473c6117f371d86d677cac475f1c16a
Jack-R-Long <jack.richard.long@gmail.com>
2021-06-14 21:48:34 -0500

    Got basic framework down.  Accepting user input for IP address.  Calling a helper function on submit

--- main.py
+++ main.py
@@ -19,22 +19,32 @@ def main():
 
     layout = [
         [sg.Text('Enter your CIDR IP!', size=(30, 1), font=("Helvetica", 25))],
-        [sg.Text('Here is some text.... and a place to enter text')],
+        # [sg.Text('Here is some text.... and a place to enter text')],
         [sg.InputText('255.255.255.255', key='in1'),
-        sg.Combo(('Combobox 1', 'Combobox 2'), key='combo', size=(20, 1))],
+        sg.Combo(('/16', '/17'), key='netBits', size=(20, 1))],
         [sg.Button('Submit')],
         [sg.Button('Exit')],
     ]
 
-    window = sg.Window('Form Fill Demonstration', layout, default_element_size=(40, 1), grab_anywhere=False)
+    window = sg.Window('Subnet Tool for Class 21015', layout, default_element_size=(40, 1), grab_anywhere=False)
 
     while True:
         event, values = window.read()
 
-        if event in ('Exit', None):
+        if event == "Submit":
+            calculateSubnet(values['in1'], values['netBits'])
+        elif event in ('Exit', None):
             break
 
     window.close()
+
+
+def calculateSubnet(ip_address, network_bits):
+    """
+    Function that will intake user IP address and calculate network ID and subnet ID
+    """
+    print(ip_address)
+    print(network_bits)
 
 
 if __name__ == '__main__':
