e7de877ce464d7799c3dc63bfb79cfb7754cab7d
Jack-R-Long <jack.richard.long@gmail.com>
2021-06-14 23:19:15 -0500

    Version 1 complete.  Tool takes user input and output corrects IP, subnnet ID, and number of hosts.  Still need to validate user input

--- main.py
+++ main.py
@@ -9,23 +9,30 @@ import ipaddress
 def main():
     # sg.theme('TanBlue')
 
-    column1 = [
-        [sg.Text('Column 1', background_color=sg.DEFAULT_BACKGROUND_COLOR,
-              justification='center', size=(10, 1))],
-        [sg.Spin(values=('Spin Box 1', '2', '3'),
-                 initial_value='Spin Box 1', key='spin1')],
-        [sg.Spin(values=('Spin Box 1', '2', '3'),
-                 initial_value='Spin Box 2', key='spin2')],
-        [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3', key='spin3')]]
-
-    layout = [
-        [sg.Text('Enter your !', size=(30, 1), font=("Helvetica", 25))],
+    input_column = [
+        [sg.Text('Enter your CIDR IP!', size=(30, 1), font=("Helvetica", 25))],
         # [sg.Text('Here is some text.... and a place to enter text')],
         [sg.InputText('192.0.2.1', key='ipAddress')],
         # sg.InputText('255.255.255.255', key='subnetMask')],
         [sg.Combo(('/1', '/2', '/3', '/4', '/5', '/6', '/7', '/8', '/9', '/10', '/11', '/12', '/13', '/14', '/15', '/16', '/17', '/18', '/19', '/20', '/21', '/22', '/23', '/24', '/25', '/26', '/27', '/28', '/29', '/30', '/31', '/32'), key='netBits', size=(20, 1))],
         [sg.Button('Submit')],
         [sg.Button('Exit')],
+    ]
+    ouput_data_column = [
+        [sg.Text("IP Address"),
+        sg.Text(size=(40, 1), key="ipOut")],
+        [sg.Text("Subnet ID"),
+        sg.Text(size=(40, 1), key="subnetOut")],
+        [sg.Text("Number of hosts"),
+        sg.Text(size=(40, 1), key="numHostsOut")],
+    ]
+
+    layout = [
+        [
+            sg.Column(input_column),
+            sg.VSeparator(),
+            sg.Column(ouput_data_column),
+        ]
     ]
 
     window = sg.Window('Subnet Tool for Class 21015', layout, default_element_size=(40, 1), grab_anywhere=False)
@@ -34,7 +41,11 @@ def main():
         event, values = window.read()
 
         if event == "Submit":
-            calculateSubnet(values['ipAddress'], values['netBits'])
+            ipOut, subnetOut, numHostsOut = calculateSubnet(values['ipAddress'], values['netBits'])
+            window['ipOut'].update(ipOut)
+            window['subnetOut'].update(subnetOut)
+            window['numHostsOut'].update(numHostsOut)
+
         elif event in ('Exit', None):
             break
 
@@ -46,8 +57,11 @@ def calculateSubnet(ip_address, netBits):
     Function that will intake user IP address and calculate network ID and subnet ID
     """
     host1 = ipaddress.ip_interface(ip_address + netBits)
+    subnet1 = ipaddress.ip_network(host1.network)
     print('IP:', host1)
-    print('Network:', host1.network)
+    print('Subnet ID:', subnet1)
+    print('Number of possible hosts:', subnet1.num_addresses)
+    return host1, subnet1, subnet1.num_addresses
 
 
 if __name__ == '__main__':
